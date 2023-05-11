1f314091c962e0142f35d610e8a624d4d5e480c1
InwZa5412 <venresdg@gmail.com>
2023-05-11 16:46:17 +0700

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors =require('cors')
+const mysql=require('mysql2')
+require('dotenv').config()
+const app = express()
+
+app.use(cors())
+
+const connection=mysql.createConnection(process.env.DATABASE_URL)
+
+app.get('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+
+app.get('/news',(req,res)=>{
+    connection.query(
+        'SELECT * FROM news',
+        function(err ,results, fields){
+            console.log(results)
+            res.send(results)
+
+        }
+    )
+
+})
+
+app.listen(process.env.PORT || 3000)
